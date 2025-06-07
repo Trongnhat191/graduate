2f5cad3879b69b1bc328797170693c466ad6774c
Trongnhat191 <trongnhat191@gmail.com>
2025-06-07 17:39:58 +0700

    system route

--- code/frontend/src/containers/Header/menuApp.js
+++ code/frontend/src/containers/Header/menuApp.js
@@ -2,13 +2,11 @@ export const adminMenu = [
     { //hệ thống
         name: 'Hệ thống', menus: [
             {
-                name: 'Quản trị hệ thống',
-                subMenus: [
-                    { name: 'Quản lý người sử dụng', link: '/system/user-manage' },
-                    { name: 'Quản lý gói', link: '/system/product-manage' },
-                    { name: 'Đăng ký gói dịch vụ cho nhóm/tài khoản', link: '/system/register-package-group-or-account' },
-                ]
+                name: 'Quản trị hệ thống', link: '/system/user-manage', 
             },
+            // {
+            //     name: "Thống kê doanh thu", link: '/system/statistic-revenue',
+            // }
             // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
         ]
     },
